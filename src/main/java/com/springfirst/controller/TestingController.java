f29f9605d242d809e871483ad199917a2c1cdd8c
Muhannad-abu-jaish <mohnad20211@gmail.com>
2024-03-21 15:20:36 +0300

    Spring init

---
+++ src/main/java/com/springfirst/controller/TestingController.java
@@ -0,0 +1,23 @@
+package com.springfirst.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//The main url
+//http://localhost:8080
+@RestController
+@RequestMapping("/api")
+
+//The url to enter this class
+//http://localhost:8080/api
+public class TestingController {
+
+    //The url to enter this function
+    //http://localhost:8080/api/name
+    @GetMapping("/name")
+    public String getName()
+    {
+        return "i am muhannad" ;
+    }
+}
